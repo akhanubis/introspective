5ff04d82bec667c4b3f9bcbd04bea6313a3fec50
Pablo <bianciottopablo@gmail.com>
2013-01-21 21:03:41 -0300

    added monkeypatch to avoid creating a singleton_class when browsing the method_lookup_path of an object

--- introspective/method_locator.rb
+++ introspective/method_locator.rb
@@ -2,7 +2,8 @@ module MethodLocator
   #monkeypath of method_locator gem to avoid forcing creation of singleton class
   private
   def fetch_singleton_class
-    if self == self.klass.attached_object
+    #dont fetch the singleton class unless it has already been created
+    if self.klass.is_singleton && self == self.klass.attached_object
       self.singleton_class
     else
       nil
